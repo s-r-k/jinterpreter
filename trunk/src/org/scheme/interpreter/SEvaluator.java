fa47e7196bf58819bf9477f492585c4314fc9a80
srihari.srinivasan <srihari.srinivasan@0248d00a-4a35-0410-b76d-a534ee91dd6f>
2007-10-23 16:55:24 +0000

    Hari - Starting with self evaluating expressions

--- trunk/src/org/scheme/interpreter/SEvaluator.java
+++ trunk/src/org/scheme/interpreter/SEvaluator.java
@@ -1,13 +1,20 @@
 package org.scheme.interpreter;
 
 public class SEvaluator {
+    private static final String DECIMAL_NUMBER = "\\d+\\.?\\d+";
+    private static final String ALPHA_NUMERIC = "\\w+";
 
     /**
      * Eval takes as arguments an expression and an environment.
      * It classifies the expression and directs its evaluation.
      */
-    public Object eval(SExpression expression, GlobalEnvironment root) {
-
+    public Object eval(String expression, GlobalEnvironment root) {
+        if (isSelfEvaluating(expression)) return expression;
         return null;
+    }
+
+    boolean isSelfEvaluating(String expression) {
+        return expression.matches(ALPHA_NUMERIC)
+                || expression.matches(DECIMAL_NUMBER);
     }
 }
--- trunk/test/org/scheme/interpreter/EvaluatorTest.java
+++ trunk/test/org/scheme/interpreter/EvaluatorTest.java
@@ -4,10 +4,18 @@ import junit.framework.TestCase;
 
 public class EvaluatorTest extends TestCase {
 
-    public void testSelfEvaluatingExpressions() {
+    public void testNumericLiterals() {
         SEvaluator evaluator = new SEvaluator();
-        Object result = evaluator.eval(new SPrimitiveExpression(), new GlobalEnvironment());
-        assertEquals(1, result);
+        Object result = evaluator.eval("1", new GlobalEnvironment());
+        assertEquals("1", result);
+        result = evaluator.eval("2.5", new GlobalEnvironment());
+        assertEquals("2.5", result);
+    }
+
+    public void testStringLiterals() {
+        SEvaluator evaluator = new SEvaluator();
+        Object result = evaluator.eval("Test", new GlobalEnvironment());
+        assertEquals("Test", result);
     }
 
 }
